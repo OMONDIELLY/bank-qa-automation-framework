dbb28a56c6275ff45bfe26eef06b04ed40ba8ed5
OMONDIELLY <obarelly98@gmail.com>
2026-04-19 17:11:49 +0300

    updated base

--- src/test/java/com/imbank/base/BaseTest.java
+++ src/test/java/com/imbank/base/BaseTest.java
@@ -1,5 +1,29 @@
 package com.imbank.base;
+import com.microsoft.playwright.*;
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;  
+
 
 public class BaseTest {
+    protected static Playwright playwright;
+    protected static Browser browser;
+    protected static Page page;
+    protected static RequestSpecification reqSpec;
 
+    @BeforeSuite
+    public void setup() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+        page = browser.newPage();
+        reqSpec = RestAssured.given().baseUri("https://api.bank.co.ke/v2");
+        
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        if (page != null) page.close();
+        if (browser != null) browser.close();
+    }
 }
